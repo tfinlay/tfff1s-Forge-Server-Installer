9b6fafb3752b9b9b746e3e00bd59b8f4b3ddf07c
Tfff1 <tfinlay2011@gmail.com>
2016-03-25 18:18:31 +1300

    added Features

---
+++ Forge Server Installer/Forge Server Installer/Select_Folder.vb
@@ -0,0 +1,36 @@
+﻿Imports System.IO
+
+Public Class Select_Folder
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Hide()
+        Form1.Show()
+        Close()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Hide()
+        Edit.Show()
+        Close()
+    End Sub
+
+    Private Sub browsebutton_Click(sender As Object, e As EventArgs) Handles browsebutton.Click
+        If (FolderBrowserDialog1.ShowDialog() = DialogResult.OK) Then
+
+            If File.Exists(FolderBrowserDialog1.SelectedPath + "\server.properties") Then
+                My.Settings.path = FolderBrowserDialog1.SelectedPath
+                My.Settings.Save()
+                pathtext.Text = FolderBrowserDialog1.SelectedPath
+                Button1.Enabled = True
+            Else
+                Button1.Enabled = False
+                MsgBox("Failed to find server.properties in the selected Folder, please select one where a server has been installed.")
+                pathtext.Text = ""
+            End If
+
+        End If
+    End Sub
+
+    Private Sub Select_Folder_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Button1.Enabled = False
+    End Sub
+End Class
